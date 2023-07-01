c48d6618b30aab9901f0e30588fb8f71d497944a
StanoSpodniak <stano28@gmail.com>
2023-07-01 14:20:32 +0200

    added comments

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from './Login';
 import Dashboard from './Dashboard';
 
+//Tutorial_1 = https://www.youtube.com/watch?v=Xcet6msf3eE stopped on 16 minute
+//Tutorial_2 = https://www.youtube.com/watch?v=1PWDxgqLmDA
+
 const code = new URLSearchParams(window.location.search).get('code');
 
 function App() {
